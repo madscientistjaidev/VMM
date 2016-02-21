0c916b50ab810b013da18f85ccd5f8c0e63fbbd7
Mad Scientist <madscientistjaidev@gmail.com>
2016-02-20 23:40:30 -0500

    Created types

--- vmm/vmm.c
+++ vmm/vmm.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum boolean{true, false};
+
+typedef struct page
+{
+    int pID;
+    char data[256];
+};
+
+typedef struct frame
+{
+    int frameNo;
+};
+
+frame phyMem[256];
+int tlb[16][2];
+page pgT[256];  
 
 int main(int argc, char** argv)
 {
+    const int mode = argc;
+    const char *inPath = argv[0];
+    const char *outPath = argv[1];
+    
+    if(FILE *fopen(inPath, "r")==NULL)
+        printf("Unable to open input file.");
+    
+    if(FILE *fopen(outPath, "w")==NULL)
+        printf("Unable to create output file.");
+    
     return (EXIT_SUCCESS);
 }
